e46fe42854c8d7956ee208ac998c7792b5480e51
limingbonc <1322569042@qq.com>
2018-08-01 00:06:01 +0800

    算法

---
+++ suanfa/src/main/java/com/test/suanfa/method/sushu.java
@@ -0,0 +1,16 @@
+package com.test.suanfa.method;
+
+import org.junit.Test;
+
+import static java.lang.Math.sqrt;
+
+public class sushu {
+
+  @Test   //sqrt 求平方根
+  public void test(){
+      int i =1;
+      int k ;
+      k = (int) sqrt(i);
+      System.out.println(k);
+  }
+}
